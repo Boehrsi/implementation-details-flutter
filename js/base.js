c5c30c0530c3a50a7fe5aa2d182033f0a4edafbc
Boehrsi <Boehrsi@users.noreply.github.com>
2020-09-12 15:03:41 +0000

    deploy: 2218c50673f62198545e045c473f1ecc0a0b2616

---
+++ js/base.js
@@ -0,0 +1,12 @@
+function toggleScrollingOnMobileNavigation() {
+    var checkBox = document.getElementById("toggle-nav");
+    var main = document.getElementsByTagName("main");
+    var footer = document.getElementById("footer");
+    if (checkBox.checked == true) {
+        main[0].setAttribute("style", "display: none");
+        footer.setAttribute("style", "display: none");
+    } else {
+        main[0].setAttribute("style", "display: block");
+        footer.setAttribute("style", "display: block");
+    }
+} 

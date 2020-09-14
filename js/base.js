7878add5172a9f0d90bdb97f9b0a33f250b82969
Boehrsi <Boehrsi@users.noreply.github.com>
2020-09-14 14:18:43 +0000

    deploy: 4e007e5f164f35f0d16b0c2cc5504db0c2480921

--- js/base.js
+++ js/base.js
@@ -1,12 +1,12 @@
 function toggleScrollingOnMobileNavigation() {
     var checkBox = document.getElementById("toggle-nav");
     var main = document.getElementsByTagName("main");
-    var footer = document.getElementById("footer");
+    var footer = document.getElementsByTagName("footer");
     if (checkBox.checked == true) {
         main[0].setAttribute("style", "display: none");
-        footer.setAttribute("style", "display: none");
+        footer[0].setAttribute("style", "display: none");
     } else {
         main[0].setAttribute("style", "display: flex");
-        footer.setAttribute("style", "display: block");
+        footer[0].setAttribute("style", "display: block");
     }
 } 
